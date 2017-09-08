b3d6beadd9fbfada5b91fadf854ec225c7482f32
xiaoyingquan <xiaoyingquan1@gmail.com>
2017-09-09 07:24:23 +0800

    update delete and posts scrolls

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,16 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:create, :destroy]
 
   def index
-    @posts = Post.order("id DESC").all   # 新帖文放前面
+    @posts = Post.order("id DESC").limit(20)   # 新帖文放前面
+
+    if params[:max_id]
+      @posts = @posts.where( "id < ?", params[:max_id])
+    end
+
+    respond_to do |format|
+      format.html # 如果客户端要求 HTML，则回传 index.html.erb
+      format.js   # 如果客户端要求 JavaScript，回传 index.js.erb
+    end
   end
 
   def create
@@ -15,6 +24,8 @@ class PostsController < ApplicationController
   def destroy
     @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
     @post.destroy
+
+    render :json => { :id => @post.id }
   end
 
   def like
